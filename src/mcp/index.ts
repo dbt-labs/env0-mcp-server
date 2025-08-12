c9cca022f505a683df9b453c7b70d5ea179fc90b
Yarden Fishler <yarden.fishler@env0.com>
2025-08-12 14:55:29 +0300

    Moved to a better structure of files and configuration and used tsx instead of tsc+js

---
+++ src/mcp/index.ts
@@ -0,0 +1,14 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import {registerAddTool} from "./tools/add";
+
+
+function createServer() {
+    const server = new McpServer({
+        name: "env0-mcp-server",
+        version: "1.0.0"
+    });
+    registerAddTool(server);
+    return server;
+}
+
+export { createServer };
